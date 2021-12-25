15212222b6bcd98458c1715204a66630a63af7dd
jamshid-zayniyev <84299973+jamshid-zayniyev@users.noreply.github.com>
2021-12-26 03:05:52 +0500

    sadsa
    dsadsa

--- src/pages/Yangiliklar.js
+++ src/pages/Yangiliklar.js
@@ -28,37 +28,25 @@ const { Panel } = Collapse;
 export default class Yangiliklar extends Component {
   state = {
     loader: true,
-    News: null,
+    news: null,
     school: null,
-  };
-  constructor() {
-    super();
-    this.state = {
-      loading: true,
-      News: null,
-    };
-  }
-
-  getNews = () => {
-    getNews()
-      .then((res) => {
-      
-        this.setState({
-          News: res.data,
-          loading: false,
-        });
-      })
-     
   };
 
   componentDidMount() {
-    this.getSchool();
     this.getNews();
   }
+  getNews = () => {
+    axios.get(`${url}/news/`).then((res) => {
+      this.setState({
+        news: res.data,
+        loading: false,
+      });
+    });
+  };
   render() {
     return (
       <>
-        {this.state.loader ? (
+        {this.state.timePassed == false ? (
           <div className={style.load}>
             <GridLoader color="violet" loading={this.state.loader} size={40} />
           </div>
@@ -70,139 +58,33 @@ export default class Yangiliklar extends Component {
                 <source src={video2} type="video/mp4" />
               </video>
             </div>
-            <div className={style.binaf}>
-              {/* <h1>
-                {" "}
-                O'zbekiston Respublikasi Namangan viloyati xalq ta'limi
-                boshqarmasining yangiliklari
-              </h1> */}
-            </div>
+
+            <div className={style.binaf}></div>
             <div className={styles.newsY}>
               <Collapse accordion defaultActiveKey={["0"]}>
-                <Panel header="O‘qituvchilar uchun ilk marta reytingli olimpiada !!!">
-                  <div>
-                    <Container>
-                      <div className={styles.imag}>
-                        <img src={newspictureolimp} />
-                      </div>
-                      <div className={styles.yozuv}>
-                        <h1>
-                          O‘qituvchilar uchun ilk marta reytingli olimpiada!!!
-                        </h1>
-                        <p>
-                          <i className="fa fa-calendar"></i>2021-12-18
-                        </p>
-                        <p>
-                          www.khiso.uz saytimizda 19-dekabr soat 14:00 dan 22:00
-                          ga qadar quyidagi fanlardan o‘qituvchilar uchun
-                          attestatsiyaga tayyorgarlik testlari o‘tkaziladi:
-                          Matematika,Fizika,Kimyo,Biologiya,Geografiya,Ona tili
-                          va adabiyot,Ingliz tili,Rus tili,Tarix,Boshlang‘ich
-                          ta’lim,Huquq,Qoraqalpoq tili,Tasviriy san’at va
-                          chizmachilik,Tarbiya,Texnologiya,Informatika Soat
-                          22:00 dan keyin barcha ishtirokchilar QR kodli
-                          sertifikat yuklab olishadi.
-                        </p>
-                      </div>
-                    </Container>
-                  </div>
-                </Panel>
-                <Panel header="NAMANGANLIK OʻQUVCHI JAHON ChEMPIONATIDA 3 – OʻRINNI QOʻLGA KIRITDI">
-                  <div>
-                    <Container>
-                      <div className={styles.imag}>
-                        <img src={newspicturetakewoondo} />
-                      </div>
-                      <div className={styles.yozuv}>
-                        <h1>
-                          NAMANGANLIK OʻQUVCHI JAHON ChEMPIONATIDA 3 – OʻRINNI
-                          QOʻLGA KIRITDI
-                        </h1>
-                        <p>
-                          <i className="fa fa-calendar"></i>2021-12-18
-                        </p>
-                        <p>
-                          Belarus respublikasining Minsk shahrida tayekvondo ITF
-                          boʻyicha 14-17 yoshlilar hamda faxriylar oʻrtasida
-                          Jahon chempionati oʻtkazidi. Oʻzbekiston nomidan 21
-                          nafar sportchi ishtirok etgan nufuzli musobaqada
-                          Namangan viloyati Yangi Namangan tumanidagi 60 –
-                          umumiy oʻrta taʼlim maktabining 10 – sinf oʻquvchisi
-                          Ziyoda Abdulhamidova 3 – oʻrinni qoʻlga kiritib,
-                          bronza medaliga sazovor boʻldi. Jahon chempionatida
-                          muvaffaqiyatli ishtirok etgan Ziyodaning kelgusi
-                          ishlarida omad tilaymiz. Namangan viloyat xalq taʼlimi
-                          boshqarmasi Axborot xizmati
-                        </p>
-                      </div>
-                    </Container>
-                  </div>
-                </Panel>
-                <Panel header=" KOSONSOYLIK OʻQUVCHILAR KASBLAR YARMARKASIDA QATNASHISHDI">
-                  <div>
-                    <Container>
-                      <div className={styles.imag}>
-                        <img src={newspicturekosonsoy} />
-                      </div>
-                      <div className={styles.yozuv}>
-                        <h1>
-                          KOSONSOYLIK OʻQUVCHILAR KASBLAR YARMARKASIDA
-                          QATNASHISHDI
-                        </h1>
-                        <p>
-                          <i className="fa fa-calendar"></i>2021-12-18
-                        </p>
-                        <p>
-                          Kosonsoy tumanidagi 58 ta umumtaʼlim maktablarining
-                          7-11-sinflarida tahsil olayotgan 700 nafardan ziyod
-                          oʻquvchilar “Professional taʼlim muassalarida
-                          oʻqitilayotgan kasblar yarmarkasi”da ishtirok etdilar.
-                          Tumandagi 1-kasb-hunar maktabida oʻtkazilgan
-                          yarmarkada oʻgʻil-qizlar chevarchilik, sartaroshlik,
-                          informatsion texnologiyalar, robototexnika,
-                          chilangarlik kabi bir qator kasblarning sir-asrorlari
-                          bilan yaqindan tanishdilar. Shuningdek, kasb-hunar
-                          maktablarining koʻrgazmalari ham ayniqsa, yuqori sinf
-                          oʻquvchilari uchun ayni muddao boʻldi. Ular oʻzlari
-                          egallamoqchi boʻlgan yoʻnalishlar boʻyicha qoʻshimcha
-                          maʼlumotlarga ega boʻldilar. Namangan viloyati xalq
-                          taʼlimi boshqarmasi Axborot xizmati
-                        </p>
-                      </div>
-                    </Container>
-                  </div>
-                </Panel>
-                <Panel header="NAMANGANDA “MENING PREZIDENTIM” TANLOVINING VILOYAT BOSQICHI ISHTIROKCHILARI TARIXIY SHAHARLARGA BEPUL SAYOHAT QILADILAR">
-                  <div>
-                    <Container>
-                      <div className={styles.imag}>
-                        <img src={newspicturemeningpre} />
-                      </div>
-                      <div className={styles.yozuv}>
-                        <h1>
-                          NAMANGANDA “MENING PREZIDENTIM” TANLOVINING VILOYAT
-                          BOSQICHI ISHTIROKCHILARI TARIXIY SHAHARLARGA BEPUL
-                          SAYOHAT QILADILAR
-                        </h1>
-                        <p>
-                          <i className="fa fa-calendar"></i>2021-12-18
-                        </p>
-                        <p>
-                          Давлатобод туманидаги “Янги зиё” маърифат масканида
-                          “Менинг президентим” республика танловининг вилоят
-                          босқичи ўтказилди. Кўрик-танлов шартларига кўра эссе,
-                          иншо ва тақдимот орқали 5 йил давомида Президентимиз
-                          ташаббуси билан амалга оширилган ижтимоий-иқтисодий,
-                          маданий ва бунёдкорлик сохаларидаги ишлар, шунингдек,
-                          ҳалқаро тузилмалар доирасидаги ҳамда биродар
-                          халқларимиз ўртасидаги дўстлик, яхши қўшничилик ва
-                          стратегик шериклик муносабатларини очиб бериш кўзда
-                          тутилган.
-                        </p>
-                      </div>
-                    </Container>
-                  </div>
-                </Panel>
+                {this.state.news !== null
+                  ? this.state.news.map((item) => {
+                      return (
+                        <Panel header={item.name}>
+                          <div>
+                            <Container>
+                              <div className={styles.imag}>
+                                <img src={item.image} />
+                              </div>
+                              <div className={styles.yozuv}>
+                                <h1>{item.name}</h1>
+                                <p>
+                                  <i className="fa fa-calendar"></i>
+                                  {item.date_added}
+                                </p>
+                                <p>{item.description}</p>
+                              </div>
+                            </Container>
+                          </div>
+                        </Panel>
+                      );
+                    })
+                  : ""}
               </Collapse>
             </div>
             <Footer />
