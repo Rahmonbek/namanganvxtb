33e61323821955ab5eaaeda28c0ec12ea9d82616
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-01-10 15:31:49 +0500

    вмвыа

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://143.244.209.138:8800";
+export const url = "http://62.209.143.146:8800";
 // export const id=2
 export const httpRequest = (config) => {
   return axios({
